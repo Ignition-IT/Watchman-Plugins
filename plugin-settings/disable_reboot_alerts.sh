b6cdc2decb73d683f5ab0a46e9165ee85ab066b7
ella-ignition <61763014+ella-ignition@users.noreply.github.com>
2020-12-11 10:21:45 -0800

    Create disable_reboot_alerts.sh

---
+++ plugin-settings/disable_reboot_alerts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#modify plugin settings
+#written by Ella Hansen for Ignition www.ignitionit.com
+
+defaults write /Library/MonitoringClient/PluginSupport/check_reboot_time_settings Warn_On_Reboot -bool false
